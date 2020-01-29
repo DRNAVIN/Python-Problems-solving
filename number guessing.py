cae15011efee5a8634fea8b885013686e1192a2d
DRNAVIN <51353627+DRNAVIN@users.noreply.github.com>
2020-01-29 12:40:36 +0530

    Update number guessing.py

--- number guessing.py
+++ number guessing.py
@@ -1,7 +1,10 @@
 import random
+#IF YOU WANT TO LOOP THIS CREATE FUNCTION 
 n=int(input("enter a number:"))
 for i in range(random.randrange(0,9)):
     if(i==n):
         print("SUCESS")
     else:
         print("try again")
+        #call function here
+#call FUNCTION HERE
